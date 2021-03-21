a33e621651ba9b3ac8663e1db4626502a9517284
marcelopbg <mpbguima@gmail.com>
2021-03-21 18:59:43 -0300

    add navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,11 @@ import { HEROES } from './mock-heroes';
   providedIn: 'root'
 })
 export class HeroService {
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id) as Hero;
+    this.messageService.add(`HeroService: fetched hero id=${id}`)
+    return of(hero);
+  }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
